d29182f3a7e45bace2726452a00eda0482650e7d
UllioBra <3131367211@qq.com>
2018-08-26 00:07:22 +0800

    8.26.2018

---
+++ oi/duipai/pre/juruo.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <queue>
+#include <list>
+#include <map>
+#include <set>
+#define ll long long
+#define ull unsigned long long
+#define io_F std::ios::sync_with_stdio(false)
+using namespace std;
+ 
+int n;
+ 
+int main () {
+    cin >> n;
+    cout << n;
+    return 0;
+}
