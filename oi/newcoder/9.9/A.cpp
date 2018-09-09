3606a777a681a0acd701b87a9991003feb86ef18
UllioBra <3131367211@qq.com>
2018-09-09 23:01:47 +0800

    9.9.2018

---
+++ oi/newcoder/9.9/A.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <queue>
+#include <list>
+#include <map>
+#include <set>
+#define ll long long
+#define ull unsigned long long
+#define io_F std::ios::sync_with_stdio(false)
+using namespace std;
+
+int n, le;
+int a[100000];
+
+int main () {
+    
+    
+    return 0;
+}
