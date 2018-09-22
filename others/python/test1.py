0916bfb697d33b1f8d9a9d16207b407faff87505
UllioBra <3131367211@qq.com>
2018-09-22 16:43:58 +0800

    9.22.2018

--- others/python/test1.py
+++ others/python/test1.py
@@ -1,33 +1,11 @@
-import os
-import sys
+from flask import Flask
+app = Flask(__name__)
 
-from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QPushButton
-from PyQt5.QtGui import QIcon, QFont
 
-class test (QWidget):
-    
-    def __init__ (self):
-        super().__init__()
+@app.route('/')
+def hello_world():
+        return 'Hello World!'
 
-        self.initUI()
 
-    def initUI (self):
-        
-        QToolTip.setFont(QFont('SansSerif', 10))
-        self.setToolTip('Button')
-        butt = QPushButton('Button1', self)
-        butt.setToolTip('Button2')
-        butt.resize(butt.sizeHint())
-        butt.move(50, 50)
-
-        self.setGeometry(300, 300, 300, 220)
-        self.setWindowTitle('exciting')
-        self.setWindowIcon(QIcon('test.jpeg'))
-
-        self.show()
-
-app = QApplication(sys.argv)
-
-ex = test();
-
-sys.exit(app.exec_())
+if __name__ == '__main__':
+        app.run()
