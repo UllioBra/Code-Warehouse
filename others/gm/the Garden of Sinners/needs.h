adc7d9c9a6b5f68d6cc490898ea0039385753fbc
UllioBra <3131367211@qq.com>
2018-12-06 19:01:20 +0800

    12.5.2018

--- others/gm/the Garden of Sinners/needs.h
+++ others/gm/the Garden of Sinners/needs.h
@@ -9,4 +9,28 @@
 #include <list>
 #include <map>
 #include <set>
+#include <fstream>
 #include <windows.h>
+#include "rapidjson/document.h"
+#include "rapidjson/istreamwrapper.h"
+#include "rapidjson/filereadstream.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/rapidjson.h"
+#include "rapidjson/writer.h"
+
+namespace Random {
+
+}
+
+template <class T>
+int GetArrLen(T& arr) {return sizeof(arr) / sizeof(arr[0]); }
+
+void Delayed_Print(const std::string&a, const int&x, const int&y, const int&z) {
+    for(int i = 0; i < a.size(); i++) {
+        std::cout << a[i];
+        Sleep(x);
+    }
+    std::cout << std::endl;
+    Sleep(y);
+    if(z) system("cls");
+}
